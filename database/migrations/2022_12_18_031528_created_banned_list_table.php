fad5506233562e6773cacf35c14612442b7874fa
Robson Trasel <robson.trasel@gmail.com>
2022-12-18 00:24:24 -0300

    db: created migrations for banned list from system

---
+++ database/migrations/2022_12_18_031528_created_banned_list_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ban_list', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('status');
+            $table->string('email');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ban_list');
+    }
+};
