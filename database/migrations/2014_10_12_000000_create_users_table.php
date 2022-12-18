5ccb001ad212977cfe2ffaf97e0c95897548dacb
Robson Trasel <robson.trasel@gmail.com>
2022-12-18 00:24:52 -0300

    chore: alter users table, added some essential columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,34 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('gender');
+            $table->string('email')->unique()->nullable();
+            $table->boolean('ban');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60);
+            $table->integer('active');
+            $table->boolean('is_deleted')->default(0);
+            $table->string('ext');
+            $table->integer('country_code');
+            $table->string('phone_number');
+            $table->string('mobile')->nullable()->unique();
+            $table->text('agent_sign');
+            $table->string('account_type');
+            $table->string('account_status');
+            $table->integer('assign_group')->unsigned()->nullable()->index('assign_group_3');
+            $table->integer('primary_dpt')->unsigned()->nullable()->index('primary_dpt_2');
+            $table->string('agent_timezone');
+            $table->string('daylight_save');
+            $table->string('limit_access');
+            $table->string('directory_listing');
+            $table->string('vacation_mode');
+            $table->string('company');
+            $table->string('role');
+            $table->string('internal_note');
+            $table->string('profile_pic');
             $table->rememberToken();
             $table->timestamps();
         });
