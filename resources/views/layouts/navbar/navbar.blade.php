64f034a58d35acbf1de5a1069b5f0c2b2a0b0c2a
Robson Trasel <robson.trasel@gmail.com>
2022-12-18 00:39:32 -0300

    dev: navbar created in separated component

---
+++ resources/views/layouts/navbar/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button
+        type="button"
+        class="navbar-toggle collapsed"
+        data-toggle="collapse"
+        data-target="#navbar-collapse"
+      >
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Ribbie Desk</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse">
+      <ul class="nav navbar-nav">
+          <li><a href="#">Home</a></li>
+      </ul>
+      <ul class="nav navbar-nav">
+          <li><a href="#">Groups</a></li>
+      </ul>
+      <ul class="nav navbar-nav">
+          <li><a href="#">Departaments</a></li>
+      </ul>
+      <ul class="nav navbar-nav">
+          <li><a href="#">Teams</a></li>
+      </ul>
+      <ul class="nav navbar-nav">
+          <li><a href="#">Agents</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+
+          <li><a href="#"></a></li>
+
+      </ul>
+
+
+    </div>
+  </div>
+</nav>
